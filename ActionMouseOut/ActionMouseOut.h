7ef59c9bfed8c2e49e71de6b0a134b6148c892f7
Erik Stainsby <erik.stainsby@roaringsky.ca>
2012-02-18 21:06:32 -0800

    stubbed in

---
+++ ActionMouseOut/ActionMouseOut.h
@@ -0,0 +1,13 @@
+//
+//  ActionMouseOut.h
+//  ActionMouseOut
+//
+//  Created by Erik Stainsby on 12-02-18.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import <RSTrixiePlugin/RSTrixiePlugin.h>
+
+@interface ActionMouseOut : RSTrixiePlugin
+
+@end
---
+++ ActionMouseOut/ActionMouseOut.m
@@ -0,0 +1,27 @@
+//
+//  ActionMouseOut.m
+//  ActionMouseOut
+//
+//  Created by Erik Stainsby on 12-02-18.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import "ActionMouseOut.h"
+
+@implementation ActionMouseOut
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+	
+    if( nil != (self = [super initWithNibName:nibNameOrNil	bundle:nibBundleOrNil]))
+    {
+		[self setName: @"MouseOut"];
+    }
+    return self;
+}
+
+
+- (BOOL) hasSelectorField {
+	return YES;
+}
+
+@end
